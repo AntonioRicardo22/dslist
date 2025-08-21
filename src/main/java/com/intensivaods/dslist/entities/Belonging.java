b451445b765f644241207359c205d1b80a7a88b3
AntonioRicardo22 <toninhoravs@hotmail.com>
2025-08-21 16:29:10 -0300

    domain model

---
+++ src/main/java/com/intensivaods/dslist/entities/Belonging.java
@@ -0,0 +1,57 @@
+package com.intensivaods.dslist.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+
+@Entity
+@Table (name = "tb_belonging")
+public class Belonging {
+
+    @EmbeddedId
+    private BelongingPk id = new BelongingPk();
+
+    private Integer position;
+
+
+    public Belonging(){}
+
+    public Belonging(Game game, GameList gameList, Integer position) {
+        this.id.setGame(game);
+        this.id.setGameList(gameList);
+
+        this.position = position;
+    }
+
+    public BelongingPk getId() {
+        return id;
+    }
+
+    public void setId(BelongingPk id) {
+        this.id = id;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Belonging belonging = (Belonging) o;
+        return Objects.equals(id, belonging.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
